b4982360e83410bd791cd17a0fa6b525f3b97350
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-10-27 14:59:05 +0600

    children props

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const style = {
+    padding: "2rem",
+    borderRadius: "20px",
+    border: "0",
+    backgroundColor: "orange",
+    cursor: "pointer",
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button text="add" style={style}></Button>
+      <Button text="remove" style={style}></Button>
+
+      {/* we will do it a better way */}
+      <NewButton style={style}>First Button</NewButton>
+      <NewButton style={style}>second Button</NewButton>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+function Button({ style, text }) {
+  return <button style={style}>{text}</button>;
+}
+function NewButton({ style, children }) {
+  console.log(children);
+  return <button style={style}>{children}</button>;
+}
